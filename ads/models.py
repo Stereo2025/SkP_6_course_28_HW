0a4c33b2b7c9269b4709910e8190720164cd1b52
Stereo2025 <teneloks@gmail.com>
2022-12-15 20:05:33 +0300

    Step 1.1

--- ads/models.py
+++ ads/models.py
@@ -4,6 +4,13 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
 
 class Ad(models.Model):
     name = models.CharField(max_length=20)
@@ -13,4 +20,9 @@ class Ad(models.Model):
     address = models.CharField(max_length=200)
     is_published = models.BooleanField(default=False)
 
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
 
+    def __str__(self):
+        return self.name
